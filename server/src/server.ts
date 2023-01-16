9e7caf06de0f72d6b11ecaf87c886fd8c9b03547
¡Duque <atanaelleonardo@gmail.com>
2023-01-16 20:46:43 -0300

    feat: prisma setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,13 @@
+import { PrismaClient } from '@prisma/client';
 import Fastify from 'fastify';
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get('/', () => {
-  return 'Hello World';
+app.get('/hello', async () => {
+  const habits = await prisma.habit.findMany({});
+
+  return habits;
 });
 
 app
