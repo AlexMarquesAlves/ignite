f1af76331e9c1e8aed7281def1e5b4eaabdf1f9a
¡Duque <atanaelleonardo@gmail.com>
2023-01-17 20:36:01 -0300

    feat: created route habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,5 +1,7 @@
+import dayjs from 'dayjs';
 import { FastifyInstance } from 'fastify';
 import { z } from 'zod';
+import { prisma } from './lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/habits', async (request) => {
@@ -9,5 +11,21 @@ export async function appRoutes(app: FastifyInstance) {
     });
 
     const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf('day').toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((weekDay) => {
+            return {
+              week_day: weekDay,
+            };
+          }),
+        },
+      },
+    });
   });
 }
