e6ed8d9f5b89335ee206b37cc8a4fb69795dda10
¡Duque <atanaelleonardo@gmail.com>
2023-01-18 19:48:49 -0300

    feat: added new touchble button

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 
 import Logo from '../../assets/logo.svg';
 
@@ -7,7 +7,16 @@ export function Header() {
     <View className="w-full flex-row items-center justify-between">
       <Logo />
 
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+      >
 
+
+        <Text className="text-white ml-3 font-semibold text-base">
+          Novo
+        </Text>
+      </TouchableOpacity>
     </View>
   )
 }
