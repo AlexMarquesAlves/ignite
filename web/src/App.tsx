40ffaa8f80cfb400f0978a6e78810d8273736a75
¡Duque <atanaelleonardo@gmail.com>
2023-01-18 01:11:09 -0300

    feat: Aula 02 - ReactJS

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -10,8 +10,6 @@ export function App() {
         <Header />
         <SummaryTable />
       </div>
-
     </div>
   );
 }
-
--- web/src/components/SummaryTable/index.tsx
+++ web/src/components/SummaryTable/index.tsx
@@ -10,13 +10,13 @@ const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
-    <div className="w-full flex">
-      <div className="grid grid-rows-7 grid-flow-row gap-3">
+    <div className={"w-full flex"}>
+      <div className={"grid grid-rows-7 grid-flow-row gap-3"}>
         {weekDays.map((weekDay, i) => {
           return (
             <div
               key={`${weekDay}-${i}`}
-              className="text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"
+              className={"text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"}
             >
               {weekDay}
             </div>
@@ -24,7 +24,7 @@ export function SummaryTable() {
         })}
       </div>
 
-      <div className="grid grid-rows-7 grid-flow-col gap-3">
+      <div className={"grid grid-rows-7 grid-flow-col gap-3"}>
         {summaryDates.map((date) => {
           return <HabitDay key={date.toString()} />;
         })}
@@ -34,7 +34,7 @@ export function SummaryTable() {
             return (
               <div
                 key={i}
-                className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"
+                className={"w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"}
               />
             );
           })}
