ff785ea206df18b8d324c8fd611d5fad18ad708f
¡Duque <atanaelleonardo@gmail.com>
2023-01-18 19:52:45 -0300

    feat: finished <HabitDay /> component

--- mobile/src/components/HabitDay/index.tsx
+++ mobile/src/components/HabitDay/index.tsx
@@ -1,23 +1,17 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Dimensions, TouchableOpacity } from "react-native";
 
-export const HabitDay = () => {
+const WEEK_DAYS = 7;
+const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
+
+export const DAY_MARGIN_BETWEEN = 8;
+export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
+
+export function HabitDay() {
   return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }>HabitDay</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#312e38',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: '#fff',
-  },
-});
+    <TouchableOpacity
+      className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
+      style={{ width: DAY_SIZE, height: DAY_SIZE }}
+      activeOpacity={0.7}
+    />
+  )
+}
