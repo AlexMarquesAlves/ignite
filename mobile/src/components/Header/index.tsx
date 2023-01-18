96541d052aba3285cd26ba5b932ac734ae2f752d
¡Duque <atanaelleonardo@gmail.com>
2023-01-18 19:49:16 -0300

    feat: added plus icon on touchble button

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { Feather } from '@expo/vector-icons';
 import { Text, TouchableOpacity, View } from "react-native";
+import colors from 'tailwindcss/colors';
 
 import Logo from '../../assets/logo.svg';
 
@@ -11,7 +13,11 @@ export function Header() {
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
       >
-
+        <Feather
+          name="plus"
+          color={colors.violet[500]}
+          size={20}
+        />
 
         <Text className="text-white ml-3 font-semibold text-base">
           Novo
