ab8d0d47cdabf5a7439ba801b66c1f116f76e605
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 00:30:20 -0300

    feat: added input on <NewHabitForm /> component

--- web/src/components/NewHabitForm/index.tsx
+++ web/src/components/NewHabitForm/index.tsx
@@ -6,6 +6,18 @@ export function NewHabitForm() {
         Qual seu comprometimento?
       </label>
 
+      <input
+        type="text"
+        id="title"
+        placeholder="ex.: Exercícios, dormir bem, etc..."
+        className="p-4 mt-3 text-white rounded-lg bg-zinc-800 placeholder:text-zinc-400"
+        autoFocus
+      />
+
+      <label htmlFor="" className="mt-4 font-semibold leading-tight">
+        Qual a recorrência?
+      </label>
+
 
     </form>
   )
