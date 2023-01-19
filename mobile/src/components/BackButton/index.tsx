7ba0fbb4ad116f8c19901854c68de850f9a26ec7
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 20:59:58 -0300

    feat: added useNavigation to go back

--- mobile/src/components/BackButton/index.tsx
+++ mobile/src/components/BackButton/index.tsx
@@ -1,23 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useNavigation } from "@react-navigation/native";
+import { TouchableOpacity } from "react-native";
 
-export const BackButton = () => {
+export function BackButton() {
+  const { goBack } = useNavigation()
+
   return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }>BackButton</Text>
-    </View>
-  );
-};
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={goBack}
+    >
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#312e38',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: '#fff',
-  },
-});
+    </TouchableOpacity>
+  )
+}
