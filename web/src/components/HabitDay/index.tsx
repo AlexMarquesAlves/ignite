8cb5f78e898ff44d79623c90eeaaf5954691a0eb
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 00:46:21 -0300

    feat: added Popover on <HabitDay /> component

--- web/src/components/HabitDay/index.tsx
+++ web/src/components/HabitDay/index.tsx
@@ -1,9 +1,22 @@
-// interface HabitDayProps {
-//   completed: number
-// }
+import * as Popover from '@radix-ui/react-popover';
 
-export function HabitDay() {
+export function HabitDay({ completed, amount }) {
+  const completedPercentage = Math.round((completed / amount) * 100);
+
   return (
-    <div className={"w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg"}></div>
+    <Popover.Root>
+      <Popover.Trigger />
+
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">terça-feira</span>
+          <span className="mt-1 text-3xl font-extrabold leading-tight">
+            17/01
+          </span>
+
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
+    </Popover.Root>
   );
 }
