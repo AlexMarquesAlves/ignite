2ac53608ee33ca649657782d08167ed89a29d1c0
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 00:41:28 -0300

    feat: update <ProgressBar /> component

--- web/src/components/ProgressBar/index.tsx
+++ web/src/components/ProgressBar/index.tsx
@@ -1,7 +1,19 @@
-export const ProgressBar = () => {
+interface ProgressBarProps {
+  progress: number
+}
+
+export const ProgressBar = (props: ProgressBarProps) => {
   return (
-    <>
-      <h1>ProgressBar</h1>
-    </>
-  );
-};
+    <div className='w-full h-3 mt-4 rounded-xl bg-zinc-700'>
+      <div
+        role="progressbar"
+        aria-label="Progresso de hábitos completados nesse dia"
+        aria-valuenow={props.progress}
+        className="h-3 rounded-xl bg-violet-600"
+        style={{
+          width: `${props.progress}%`
+        }}
+      />
+    </div>
+  )
+}
