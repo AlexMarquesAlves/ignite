0abf1121c9900cb29a447bcfbbb8511d11fe2920
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 01:03:06 -0300

    feat: added dynamic properties of <HabitDay /> on <SumaryTable /> component

--- web/src/components/SummaryTable/index.tsx
+++ web/src/components/SummaryTable/index.tsx
@@ -10,13 +10,15 @@ const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
 export function SummaryTable() {
   return (
-    <div className={"w-full flex"}>
-      <div className={"grid grid-rows-7 grid-flow-row gap-3"}>
+    <div className={'w-full flex'}>
+      <div className={'grid grid-rows-7 grid-flow-row gap-3'}>
         {weekDays.map((weekDay, i) => {
           return (
             <div
               key={`${weekDay}-${i}`}
-              className={"text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center"}
+              className={
+                'text-zinc-400 text-xl h-10 w-10 font-bold flex items-center justify-center'
+              }
             >
               {weekDay}
             </div>
@@ -24,9 +26,15 @@ export function SummaryTable() {
         })}
       </div>
 
-      <div className={"grid grid-rows-7 grid-flow-col gap-3"}>
+      <div className={'grid grid-rows-7 grid-flow-col gap-3'}>
         {summaryDates.map((date) => {
-          return <HabitDay key={date.toString()} />;
+          return (
+            <HabitDay
+              key={date.toString()}
+              amount={5}
+              completed={Math.round(Math.random() * 5)}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
@@ -34,7 +42,9 @@ export function SummaryTable() {
             return (
               <div
                 key={i}
-                className={"w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed"}
+                className={
+                  'w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg opacity-40 cursor-not-allowed'
+                }
               />
             );
           })}
