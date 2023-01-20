803a1c6d2533b10b128d0c41fd858773911b1cbf
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 21:25:06 -0300

    feat: added routes on App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,34 +4,31 @@ import {
   Inter_400Regular,
   Inter_600SemiBold,
   Inter_700Bold,
-  Inter_800ExtraBold,
-  useFonts
+  Inter_800ExtraBold, useFonts
 } from '@expo-google-fonts/inter';
 import { StatusBar } from 'react-native';
 
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
-    Inter_800ExtraBold,
+    Inter_800ExtraBold
   });
 
   if (!fontsLoaded) {
-    return <Loading />;
+    return (
+      <Loading />
+    );
   }
 
   return (
     <>
-      <Home />
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </>
   );
 }
