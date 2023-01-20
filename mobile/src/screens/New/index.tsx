fbc6bb7558aee5448df5f4d8874cfd4214255336
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 21:21:56 -0300

    feat: finished <New /> screen

--- mobile/src/screens/New/index.tsx
+++ mobile/src/screens/New/index.tsx
@@ -1,23 +1,75 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+import { useState } from "react";
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import colors from "tailwindcss/colors";
 
-export const New = () => {
+import { BackButton } from "../../components/BackButton";
+import { Checkbox } from "../../components/Checkbox";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'];
+
+export function New() {
+  const [weekDays, setWeekDays] = useState<number[]>([]);
+
+  function handleToggleWeekDay(weekDayIndex: number) {
+    if (weekDays.includes(weekDayIndex)) {
+      setWeekDays(prevState => prevState.filter(weekDay => weekDay !== weekDayIndex))
+    } else {
+      setWeekDays(prevState => [...prevState, weekDayIndex])
+    }
+  }
   return (
-    <View style={ styles.container }>
-      <Text style={ styles.title }>New</Text>
-    </View>
-  );
-};
+    <View className="flex-1 px-8 pt-16 bg-background">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <BackButton />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#312e38',
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 22,
-    color: '#fff',
-  },
-});
+        <Text className="mt-6 text-3xl font-extrabold text-white">
+          Criar hábito
+        </Text>
+
+        <Text className="mt-6 text-base font-semibold text-white">
+          Qual seu comprometimento?
+        </Text>
+
+        <TextInput
+          className="h-12 pl-4 mt-3 text-white border-2 rounded-lg bg-zinc-900 border-zinc-800 focus:border-green-600"
+          placeholder="Exercícios, dormir bem, etc..."
+          placeholderTextColor={colors.zinc[400]}
+        />
+
+        <Text className="mt-4 mb-3 text-base font-semibold text-white">
+          Qual a recorrência?
+        </Text>
+
+        {
+          availableWeekDays.map((weekDay, index) => (
+            <Checkbox
+              key={weekDay}
+              title={weekDay}
+              checked={weekDays.includes(index)}
+              onPress={() => handleToggleWeekDay(index)}
+            />
+          ))
+        }
+
+        <TouchableOpacity
+          className="flex-row items-center justify-center w-full mt-6 bg-green-600 rounded-md h-14"
+          activeOpacity={0.7}
+        >
+          <Feather
+            name="check"
+            size={20}
+            color={colors.white}
+          />
+
+          <Text className="ml-2 text-base font-semibold text-white">
+            Confirmar
+          </Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </View>
+  )
+}
