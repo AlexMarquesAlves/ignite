9f5a7d175fb002c49651b0767860e8a7d4070dde
¡Duque <atanaelleonardo@gmail.com>
2023-01-20 00:05:22 -0300

    feat: added checkbox element on <NewHabitForm /> component

--- web/src/components/NewHabitForm/index.tsx
+++ web/src/components/NewHabitForm/index.tsx
@@ -1,3 +1,4 @@
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { Check } from 'phosphor-react';
 
 export function NewHabitForm() {
@@ -18,6 +19,20 @@ export function NewHabitForm() {
       <label htmlFor="" className="mt-4 font-semibold leading-tight">
         Qual a recorrência?
       </label>
+
+      <div className="flex flex-col gap-2 mt-3">
+        <Checkbox.Root className="flex items-center gap-3 group">
+          <div className="flex items-center justify-center w-8 h-8 border-2 rounded-lg bg-zinc-900 border-zin-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+          </div>
+
+          <span className="leading-tight text-white">
+            Segunda-feira
+          </span>
+        </Checkbox.Root>
+      </div>
 
       <button
         type="submit"
