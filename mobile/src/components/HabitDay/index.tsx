9445596ccc1fed5b25ee654e9af1e0c2df2b177a
¡Duque <atanaelleonardo@gmail.com>
2023-01-19 21:15:18 -0300

    feat: update <Header/> component

--- mobile/src/components/HabitDay/index.tsx
+++ mobile/src/components/HabitDay/index.tsx
@@ -1,20 +1,31 @@
-import { Dimensions, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { Feather } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
+import { Text, TouchableOpacity, View } from "react-native";
+import colors from 'tailwindcss/colors';
+import Logo from '../../assets/logo.svg';
 
-const WEEK_DAYS = 7;
-const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5;
+export function Header() {
+  const { navigate } = useNavigation()
 
-export const DAY_MARGIN_BETWEEN = 8;
-export const DAY_SIZE = (Dimensions.get('screen').width / WEEK_DAYS) - (SCREEN_HORIZONTAL_PADDING + 5);
-
-interface Props extends TouchableOpacityProps { }
-
-export function HabitDay({ ...rest }: Props) {
   return (
-    <TouchableOpacity
-      className="m-1 border-2 rounded-lg bg-zinc-900 border-zinc-800"
-      style={{ width: DAY_SIZE, height: DAY_SIZE }}
-      activeOpacity={0.7}
-      {...rest}
-    />
+    <View className="flex-row items-center justify-between w-full">
+      <Logo />
+
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="flex-row items-center px-4 border rounded-lg h-11 border-violet-500"
+        onPress={() => navigate('new')}
+      >
+        <Feather
+          name="plus"
+          color={colors.violet[500]}
+          size={20}
+        />
+
+        <Text className="ml-3 text-base font-semibold text-white">
+          Novo
+        </Text>
+      </TouchableOpacity>
+    </View>
   )
 }
