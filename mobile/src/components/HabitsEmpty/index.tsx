4658c5e48a11affb0abac1ff1a9c0f066d72a6e8
¡Duque <atanaelleonardo@gmail.com>
2023-01-22 01:15:40 -0300

    feat: update <HabitsEmpty /> component

--- mobile/src/components/HabitsEmpty/index.tsx
+++ mobile/src/components/HabitsEmpty/index.tsx
@@ -5,10 +5,10 @@ export function HabitsEmpty() {
   const { navigate } = useNavigation()
 
   return (
-    <Text className="text-base text-zinc-400">
+    <Text className="text-zinc-400 text-base">
       Você ainda não está monitorando nenhum hábito {' '}
       <Text
-        className="text-base underline text-violet-400 active:text-violet-500"
+        className="text-violet-400 text-base underline active:text-violet-500"
         onPress={() => navigate('new')}
       >
         comece cadastrando um.
